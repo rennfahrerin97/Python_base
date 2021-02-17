709676dafd5501a81e97f8ad8a7d62184e59157b
Tetiana Tsybulska KAPELOU <ttp@ils.lc>
2021-02-17 11:26:28 +0200

    add hm_1

---
+++ hm_1/hm_1_3.py
@@ -0,0 +1,13 @@
+"""
+    Объявить переменные name, age, city,
+    присвоив им значения с помощью input().
+    Вывести на экран текст типа:
+    Max from Kiev
+    He is 21
+"""
+name = input('Name: ')
+age = input('Age: ')
+city = input('City: ')
+print()
+print(name, "from", city)
+print("He is", age)
