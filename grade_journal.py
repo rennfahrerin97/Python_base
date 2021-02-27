d62def0fedc2a1a86ba24d243c67c7873dbf2d49
Tetiana Tsybulska <78359356+rennfahrerin97@users.noreply.github.com>
2021-02-27 12:18:48 +0200

    Update grade_journal.py

--- grade_journal.py
+++ grade_journal.py
@@ -23,31 +23,33 @@
         2. Фролов
         3. Иванов
 """
+def main():
 
-#
-# def main():
-#     pass
-#
-#
-# if __name__ == "__main__":
-#     main()
+    # функція приймає кількість студентів і словник з їхніми даними
+    def input_students(students):
+        list_students = {}
+        for i in range(students):
+            s = input().split(" ")
+            list_students[int(s[1])] = s[0]
+        return list_students
 
-# функція приймає кількість студентів і словник з їхніми даними
-def input_students(students):
-    list_students = {}
-    for i in range(students):
-        s = input().split(" ")
-        list_students[int(s[1])] = s[0]
-    return list_students
+    # функція приймає словник з даними студентів та сортує їх в порядку спадання. повертає відсортований словник
+    def sort_mark(list_students):
+        list_st = list(list_students.keys())
+        list_st.sort(reverse=True)
+        tmp_data = {}
+        for i in list_st:
+            tmp_data[i] = list_students[i]
+        return tmp_data
 
-def sort_mark(list_students):
-    list_st = list(list_students.keys())
-    list_st.sort()
-    tmp_data = {}
-    for i in list_st:
-        tmp_data[i] = list_students[i]
-    return tmp_data
+    students = int(input("Введите количество студентов: "))
+    print("\nВведите фамилии и оценки через пробел:")
+    grade = sort_mark(input_students(students))
+    i = 0
+    for item in grade.values():
+        if i != students:
+            i += 1
+            print(f"{i}. {item}")
 
-students = int(input("Введите количество студентов: "))
-print("\nВведите фамилии и оценки через пробел:")
-print(sort_mark(input_students(students)))
+if __name__ == "__main__":
+    main()
